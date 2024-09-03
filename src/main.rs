5c19bb4065b25591c375b0053e8daf88dfc79c8a
mohamed abd elkhalq <01147018091.m@gmail.com>
2024-09-03 22:46:15 +0300

    add simple itrateor over a currunt dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs, path::PathBuf};
+
+use anyhow::Result;
+
+fn view_content(path: &PathBuf) -> Result<()> {
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if let Some(file_name) = path.file_name() {
+            if let Some(file_name_str) = file_name.to_str() {
+                println!("{}", file_name_str);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let currun_dir = env::current_dir()?;
+    view_content(&currun_dir)
 }
